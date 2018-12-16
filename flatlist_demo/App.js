4624cca6a31d444e9642e2e9740718b81acedf5a
Thanh Danh <15520798@gm.uit.edu.vn>
2018-12-17 04:30:50 +0700

    drawe click change page

--- flatlist_demo/App.js
+++ flatlist_demo/App.js
@@ -2,21 +2,42 @@ import React, { Component } from 'react';
 import CustomFlatList from './src/components/screens/CustomFlatList';
 // import for Menu drawer
 
-import {createAppContainer,createDrawerNavigator} from 'react-navigation'
+import {createAppContainer,createDrawerNavigator,DrawerItems} from 'react-navigation'
 import HomeScreen from './src/components/screens/HomeScreen';
 import SettingScreen from './src/components/screens/SettingScreen';
-
-const MyDrawerNavigator  = createDrawerNavigator({
-  Home:{
-    screen:HomeScreen
+import { Container, Header, Body, Content } from 'native-base';
+import {StyleSheet,Image} from 'react-native'
+const MyDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: HomeScreen,
   },
-  Settings:{
-    screen:SettingScreen
-  }
-})
+  Settings: {
+    screen: SettingScreen,
+    
+  },
+},{
+  drawerPosition:'left',
+  initialRouteName:'Home',
+  // contentComponent:CustomContentComponent
+});
 
 const MyApp = createAppContainer (MyDrawerNavigator);
 
+const CustomContentComponent = (props)=>(
+  <Container>
+    <Header style={{height:150}}>
+      <Body>
+        <Image
+          style={styles.drawerImage}
+          source={require('./assets/logo.png')}
+        ></Image>
+      </Body>
+    </Header>
+    <Content>
+      <DrawerItems {...props}></DrawerItems>
+    </Content>
+  </Container>
+)
 export default class componentName extends Component {
   constructor(props) {
     super(props);
@@ -29,8 +50,15 @@ export default class componentName extends Component {
   render() {
 
     return (
-      // <CustomFlatList></CustomFlatList>
       <MyApp></MyApp>
     )
   }
 }
+
+const style = StyleSheet.create({
+  drawerImage:{
+    height:150,
+    width:150,
+    borderRadius: 75 ,
+  }
+})
--- flatlist_demo/src/components/screens/HomeScreen.js
+++ flatlist_demo/src/components/screens/HomeScreen.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { Icon, Button, Container, Header, Content, Left } from 'native-base';
-
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import {Icon, Button, Container, Header, Content, Left} from 'native-base';
 export default class HomeScreen extends Component {
-  render() {
+  render () {
     return (
-     <Container>
-         <Content contentContainerStyle={{flex:1,alignItems:'center',justifyContent:'center'}}>
-             <Text style={{padding:10}}>Home screen</Text>
-         </Content>
-     </Container>
-    )
+      <Container>
+        <Header>
+          <Left>
+                    <Icon name="ios-menu" onPress={() => this.props.navigation.openDrawer()}/>
+          </Left>
+        </Header>
+        <Content
+          contentContainerStyle={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Text style={{padding: 10, fontSize:20, color:'#539'}}>Home screen</Text>
+        </Content>
+      </Container>
+    );
   }
 }
