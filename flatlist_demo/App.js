2966cf1d8d2fc2bb27def3fc7448f0c8152a3593
Thanh Danh <15520798@gm.uit.edu.vn>
2018-12-17 02:30:09 +0700

    flatlist demo native-base

---
+++ flatlist_demo/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import CustomFlatList from './src/components/screens/CustomFlatList';
+
+export default class componentName extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+    };
+  }
+
+
+  render() {
+
+    return (
+      <CustomFlatList></CustomFlatList>
+    )
+  }
+}
